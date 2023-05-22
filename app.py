71d5cec83a6412d94614546b33cf5cafddb33a24
b-sachin <sachinbhopiofficial@gmail.com>
2023-05-22 17:31:04 +0530

    exception handling code completed

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from housing.logger import lg
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 @app.route("/",methods = ["GET","POST"])
 def sample():
-    lg.info("We are testing logging module")
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        # housing = HousingException(e, sys)
+        raise HousingException(e, sys) from e
+        lg.info(housing.error_message)
+        lg.info("We are testing logging module")
     return "This is ML Project. This content added to test working of CI/CD pipeline."
 
 if __name__=="__main__":
