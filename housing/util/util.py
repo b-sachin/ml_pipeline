d8d830972295bdc329556f6531c530d52d646977
b-sachin <sachinbhopiofficial@gmail.com>
2023-06-09 20:09:15 +0530

    util.py updated with load_data() save_numpy_array_data() save_object()

--- housing/util/util.py
+++ housing/util/util.py
@@ -13,4 +13,55 @@ def read_yaml_file(file_path:str)->dict:
     except Exception as e:
         raise HousingException(e,sys) from e
 
-        
+
+def load_data(file_path:str, schema_file_path: str) -> pd.DataFrame:
+        try:
+            dataset_schema = read_yaml_file(schema_file_path)
+            schema = dataset_schema[DATASET_SCHEMA_COLUMNS]
+
+            dataframe = pd.read_csv(file_path)
+
+            error_message = ""
+
+            for i in dataframe.columns:
+                if i in list(schema.keys()):
+                    dataframe[i].astype(schema[i])
+                else:
+                    error_message = f"{error_message} \nColumn: [{i}] is not in the schema. "
+
+            if len(error_message)>0:
+                raise Exception(error_message)
+            return dataframe
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+
+def save_numpy_array_data(file_path:str, array:np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok = True)
+        with open(file_path,"wb") as file_obj:
+            np.save(file_obj,array)
+    except Exception as e:
+        raise HousingException(e,sys) from e
+
+
+def save_object(file_path:str, obj):
+    """
+    Save obj to pickle file
+    file_path: str location of file to save
+    obj: object to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok = True)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+    except Exception as e:
+        raise HousingException(e,sys) from e
