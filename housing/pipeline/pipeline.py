3c612b5f33d1e13b1f760c8cbb6914127a683c7b
b-sachin <sachinbhopiofficial@gmail.com>
2023-06-09 20:30:49 +0530

    pipeline.py file updated with data transformation code

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -31,9 +31,24 @@ class Pipeline:
         except Exception as e:
             raise HousingException(e,sys) from e
 
+    def start_data_transformation(self,
+                                  data_ingstion_artifact:DataIngestionArtifact,
+                                  data_validation_artifact:DataValidationArtifact
+                                  ) -> DataTransformationArtifact:
+        try:
+            data_transformation = DataTransformation(data_transformation_config=self.config.get_data_transformation_config(),
+                                                     data_ingestion_artifact=data_ingstion_artifact,
+                                                     data_validation_artifact=data_validation_artifact)
+
+            return data_transformation.initiate_data_transformation()
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_ingstion_artifact=data_ingestion_artifact,
+                                                                          data_validation_artifact=data_validation_artifact)
         except Exception as e:
             raise HousingException(e,sys) from e
