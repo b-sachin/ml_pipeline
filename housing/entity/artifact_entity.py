8a9bf57b67920fe5247ad053912b3ba4faeab401
Sachin <sachinbhopiofficial@gmail.com>
2023-06-15 10:21:01 +0530

    ModelPusherArtifact entity added

--- housing/entity/artifact_entity.py
+++ housing/entity/artifact_entity.py
@@ -27,3 +27,5 @@ ModelTrainerArtifact = namedtuple("ModelTrainerArtifact",[ "is_trained",
                                                            "model_accuracy"])
 
 ModelEvaluationArtifact = namedtuple("ModelEvaluationArtifact", ["is_model_accepted", "evaluated_model_path"])
+
+ModelPusherArtifact = namedtuple("ModelPusherArtifact", ["is_model_pusher", "export_model_file_path"])
